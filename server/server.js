406ebd088e9fc64308fb72194c6d0860499bfa5b
GaneshGupta3 <ganesh310104@gmail.com>
2025-05-27 20:44:36 +0530

    added vercel.json

--- server/server.js
+++ server/server.js
@@ -37,7 +37,7 @@ app.get('/api/streaks', async (req, res) => {
   }
 });
 
-app.get("/" , (req, res) => {
+app.get("/api/" , (req, res) => {
   res.send("Welcome to the Streak Maintainer API!");
 });
 
